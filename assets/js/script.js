604e2821bcea0a72941d32d1ab9f9eaa7bd1f4d7
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2021-07-28 16:46:34 -0400

    added fetch to NYTimes API got articles results added id to input field and retrieved its value dynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,37 @@ var searchBtn = $("#search-usa-medalist");
 //DATA =========================================================================
 
 //FUNCTIONS ====================================================================
+function searchNews(searchTerm) {
 
+    constructedNEWSURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&api-key=GGFWJmCqMq0NCTFkqwJ3PAgGRrCDZmlJ"
+    fetch(constructedNEWSURL, {
+        method: "GET", //GET is the default.
+        credentials: "same-origin", // include, *same-origin, omit
+        redirect: "follow", // manual, *follow, error
+        })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log("===== SEARCH RESULTS: =====");
+            console.log(data);
+        })
+        .catch(function (err) {
+            alert("Please make sure you entered a search term.");
+
+        });
+
+}
 
 //USER INTERACTIONS ============================================================
 
 //INITIALIZATION ===============================================================
 searchBtn.on("click", function(event){
-    console.log("search button clicked, working");
     event.preventDefault();
+    var searchBtnInput = $("#searchInput").val()
+    if (searchBtnInput) {
+        searchNews(searchBtnInput);
+    } else {
+        alert("Please enter a search term.");
+    }
 });
